f0ae9d0164d22940f1186de5585f8c06a3ce503f
沐光於江湖 <140484436+intnetcn@users.noreply.github.com>
2023-08-29 12:34:53 +0800

    Add files via upload

---
+++ admin/foot.php
@@ -0,0 +1,1 @@
+<div id="footer">Copyright &copy; <span style="color:#FF5200">沐光</span><span style="color:#000000">於</span><span style="color:#0065FF">江湖</span></div>
---
+++ admin/inc.php
@@ -0,0 +1,5 @@
+<title>沐光於江湖管理后台</title>
+<meta name="keywords" content="沐光於江湖" />
+<meta name="description" content="沐光於江湖" />
+<link href="./images/woaik.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="../img/logo.png">
