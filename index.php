93c9d0048932774b3b4df246cd13c3d1b5473fed
沐光於江湖 <140484436+intnetcn@users.noreply.github.com>
2023-08-29 12:40:44 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+﻿﻿<?php 
+
+include('inc/aik.config.php');?>
+<?='<!DOCTYPE HTML>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0"/>
+<meta name="apple-mobile-web-app-capable" content="yes"/>
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+<link rel="shortcut icon" href="images/so.png" type="image/x-icon">
+<link rel="Bookmark" href="images/so.png" >
+
+<title>'; echo $aik['title'];?><?="</title>
+<meta name=\"keywords\" content=\""; echo $aik['keywords'];?><?='">
+<meta name="description" content="'; echo $aik['description'];?><?='">
+
+<link href="style.css"     	rel="stylesheet" type="text/css">
+
+<style type="text/css">
+<!--
+.STYLE3 {
+	color: #CC3333;
+	font-size: 20px;
+	font-family: "浪漫雅圆";
+}
+-->
+</style>
+</head>
+<body>
+<div style="background-color:#e34e47;height:5px;width:100%;display:block;margin-bottom:30px;"></div>
+
+<div class="landing-page">
+	<a style="margin-bottom:30px;width:200px;margin-left:auto;margin-right:auto;" href="/">
+    	<img src="'; echo $aik['logo_dh'];?>" alt="img">
+    </a>
+    
+    <div class="decoration"></div>
+      
+          
+    <div class="landing-navigation">
+    <div class="landing-navigation-tablet-top-adjustment"></div> 
+	
+    	<a href="<?php echo $aik['1lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/daohang.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/tyys.jpg" alt="img">
+            <em><?php echo $aik['1zi'];?></em>
+        </a>	
+    	<a href="<?php echo $aik['2lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="images/index.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/index.jpg" alt="img">
+            <em><?php echo $aik['2zi'];?></em>
+        </a>
+    	<a href="<?php echo $aik['3lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/tv.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/tv.jpg" alt="img">
+            <em><?php echo $aik['3zi'];?></em>
+        </a>
+    	<a href="<?php echo $aik['4lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/movie.png" alt="img">
+            <div class="nav-overlay"></div>    
+        	<img class="nav-image" src="images/m.jpg" alt="img">
+            <em><?php echo $aik['4zi'];?></em>
+        </a>
+    	<a href="<?php echo $aik['5lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/zongyi.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/zy.jpg" alt="img">
+            <em><?php echo $aik['5zi'];?></em>
+        </a>
+        <a href="<?php echo $aik['6lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/dongman.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/dm.jpg" alt="img">
+            <em><?php echo $aik['6zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['7lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="images/dp.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/dp.jpg" alt="img">
+            <em><?php echo $aik['7zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['8lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="images/zb.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/zb.jpg" alt="img">
+            <em><?php echo $aik['8zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['9lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="images/so.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="images/yy.jpg" alt="img">
+            <em><?php echo $aik['9zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['10lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/zl.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/zl2.jpg" alt="img">
+            <em><?php echo $aik['10zi'];?></em>
+        </a>
+        <a href="<?php echo $aik['11lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/cfsl.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/cfsl.jpg" alt="img">
+            <em><?php echo $aik['11zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['12lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/qy.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/qy.jpg" alt="img">
+            <em><?php echo $aik['12zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['13lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/xlz.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/xlz.jpg" alt="img">
+            <em><?php echo $aik['13zi'];?></em>
+        </a>
+    	<a href="<?php echo $aik['14lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/jjdy.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/jjdy.jpg" alt="img">
+            <em><?php echo $aik['14zi'];?></em>
+        </a>
+        <a href="<?php echo $aik['15lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/xdlm.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/xdlm.jpg" alt="img">
+            <em><?php echo $aik['15zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['16lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/ssss.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/ssss.jpg" alt="img">
+            <em><?php echo $aik['16zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['17lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/jzz.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/jzz.jpg" alt="img">
+            <em><?php echo $aik['17zi'];?></em>
+        </a>
+	<a href="<?php echo $aik['18lianjie'];?>" target="_blank">
+        	<img class="nav-icon" src="img/mjz.png" alt="img">
+            <div class="nav-overlay"></div>
+        	<img class="nav-image" src="img/mjz.jpg" alt="img">
+            <em><?php echo $aik['18zi'];?></em>
+        </a>
+		
+        <div class="clear"></div>
+        <div class="landing-navigation-tablet-bottom-adjustment"></div> 
+    </div>
+
+    <div class="decoration"></div>
+ <div style="display:none">
+ 
+ </div>
+  
+</div>
+
+<div style="background-color:#e34e47;height:5px;width:100%;display:block;margin-top:30px;"></div>
+
+</body>
+</html>
